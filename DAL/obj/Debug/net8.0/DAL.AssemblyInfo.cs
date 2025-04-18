24aed0efbbb82c190ce0aa64371477cc8a4ffc87
Mykyta-S1 <mykyta_siniavin@epam.com>
2025-04-18 16:56:12 +0300

    git ignore update

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2b180f35ffcdaa9d8dbd122d361729b7d7ec5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
