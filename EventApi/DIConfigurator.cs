22e448b455640ec7604809c8cde0b84d42bf5d28
Mykyta-S1 <mykyta_siniavin@epam.com>
2025-04-23 13:11:37 +0300

    Added Logic level

---
+++ EventApi/DIConfigurator.cs
@@ -0,0 +1,10 @@
+﻿namespace EventApi
+{
+    public static class DIConfigurator
+    {
+        public static void ConfigureDI(WebApplicationBuilder builder)
+        {
+            //builder.Services.AddTransient<IPricingService, PricingService>();
+        }
+    }
+}
